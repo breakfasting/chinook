4669e3dbed570e250b0f73abb53518a68a5413dc
Shaphen <swpburn@gmail.com>
2020-03-15 17:56:18 -0700

    IT WORKS NOW

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -11,6 +11,10 @@ require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  # subject(:genre) { Genre.first }
+  subject(:genre) { Genre.first }
+
+  it "has tracks" do
+    expect(genre.tracks.first.name).to eq("For Those About To Rock (We Salute You)")
+  end
 
 end
